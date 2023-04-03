ea42101c06f2c2c8043c9851d096fb32e06d379e
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-04-03 13:50:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,13 @@
 import streamlit as st
+from sqlalchemy import create_engine 
+import pymysql
 
+def connect():
+   engine = create_engine('mysql+pymysql://username:password@host/dbname')  
+   return engine
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
-
-#conn = init_connection()
-
-@st.cache(allow_output_mutation=True)
-def get_connection():
-    return create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
-    fast_executemany = True
-    )
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-#rows = run_query("SELECT * from mytable;")
-
-# Print results.
-#for row in rows:
-    #st.write(f"{row[0]} has a :{row[1]}:")
-st.write(get_connection())
+eng = connect()
+con = eng.cursor()
+query="SELECT * from instructors;"
+con.execute(query)
+st.write(con.fetchall())
