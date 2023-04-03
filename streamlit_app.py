cdc9eb391f5a5936ef564a28cc18f6081dc24c84
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-04-03 14:36:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,9 @@ import streamlit as st
 from sqlalchemy import create_engine 
 import pymysql
 
-def connect():
-   engine = create_engine('mysql+pymysql://username:password@host/dbname')  
-   return engine
 
-eng = connect()
-con = eng.cursor()
+engine = create_engine('mysql+pymysql://username:password@host/dbname')  
+con = engine.cursor()
 query="SELECT * from instructors;"
 con.execute(query)
 st.write(con.fetchall())
