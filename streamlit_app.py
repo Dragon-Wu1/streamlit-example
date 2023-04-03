d16ed964252f44565ef93e540d5d6e601012aa80
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-04-03 13:15:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,13 @@ import mysql.connector
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
-conn = init_connection()
+#conn = init_connection()
+
+@st.cache(allow_output_mutation=True)
+def get_connection():
+    return create_engine("mssql+pyodbc://username:a098765@localhost/course_management?driver=ODBC+Driver+17+for+SQL+Server", 
+    fast_executemany = True
+    )
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -17,9 +23,9 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+#rows = run_query("SELECT * from mytable;")
 
 # Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
-
+#for row in rows:
+    #st.write(f"{row[0]} has a :{row[1]}:")
+st.write(get_connection())
