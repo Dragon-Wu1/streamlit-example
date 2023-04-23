3575ee4ba73913bde051d1005a9a4d48c4299769
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-04-24 04:38:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
-import psycopg2
+import snowflake.connector
 
-@st.cache_resource
 def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
 
 conn = init_connection()
 
+@st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
-      
+
 rows = run_query("SELECT * from instructors;")
+
+
 for row in rows:
     st.write(f"{row[0]} has a :{row[1]}:")
