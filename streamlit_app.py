a13a41b34e29a9ced25be9bb966bcd33d5aa439f
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-04-25 15:37:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,9 +66,8 @@ def LoggedIn_Clicked(userName, password):
     cursor.execute(sql)
     conn.commit()
     df2 = cursor.fetchone()
-    st.write(password)
-    st.write(df2[0])
-    time.sleep(3)
+    st.write(type(password))
+    st.write(type(df2[0]))
     if df2 is not None and df2[0] == password:
         st.session_state['loggedIn'] = True
     else:
