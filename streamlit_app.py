9b84e02ed20916e3aad3db1f751bbb4a5a6d945c
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-04-30 01:50:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -341,8 +341,8 @@ def show_main_page():
         image = Image.open('UM_logo.jpg')
         st.image(image)
         df2 = Read()   #return is tuples
-        dfcourse = df2['course'].values.tolist()
-        dfstatus = df2['status'].values.tolist()
+        dfcourse = df2.course
+        dfstatus = df2.status
         st.write(dfcourse)
         st.write(dfstatus)
         left_column, right_column = st.columns([2, 2])
@@ -350,7 +350,7 @@ def show_main_page():
             st.table(df2)
         with right_column:
             st.write('modify')
-            for i, j in zip(dfcourse, dfstatus):    #i is string
+            for i, j in zip(df2.course, df2.status):    #i is string
                 i = st.button(j, key=i, on_click=main_click, args=i)
         add = st.button('add', on_click=Main_Add_click)      #st.session_state['User']
         show_logout_page()
