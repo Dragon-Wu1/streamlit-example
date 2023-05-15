59ce23908ec0914b4ef720abdc8a46a6af3ff0d0
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-05-15 09:48:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,6 +128,7 @@ def LoggedIn_Clicked(userName, password):
     df2 = cursor.fetchone()
     #st.write(password)
     #st.write(type(password))
+    password = password
     df = str(df2[3])
     #password = int(password) #as password is a string and df2 is tuple
     if df2 is not None and df == password:
